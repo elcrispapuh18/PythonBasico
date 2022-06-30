4a1bbd2592e8177edd0f8834b4b5fd1569760aeb
elcrispapuh18 <elcrispapuh123@gmail.com>
2022-06-29 19:15:56 -0500

    Segundo Commit

--- conversor.py
+++ conversor.py
@@ -1,1 +1,7 @@
-print("Holaa");
+pesos = input ("Cuantos pesos colombianos tienes?")
+pesos = float(pesos)
+valor_dolar =3875
+dolares = pesos / valor_dolar
+dolores = round (dolores, 2)
+dolares = str(dolores)
+print("Tienes $" + dolares + "dolares")
