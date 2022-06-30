e2e6ed44bbca2ed76b14be450916b4bdfed74cc3
elcrispapuh18 <elcrispapuh123@gmail.com>
2022-06-29 19:29:48 -0500

    Cuarto Commit

---
+++ condicional.py
@@ -0,0 +1,12 @@
+# edad = int(input("Ingresa tu edad: "))
+# if edad > 17:
+#     print('Eres mayor de edad')
+# else:
+#     print('Eres menor de edad')
+numero= (input('Ingresa un número para darte un resultado: '))
+if numero > 5:
+    print('Es mayor a 5') 
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
