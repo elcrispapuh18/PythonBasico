8d0e4a3770f43181e0660393d203ef6a2fa13be8
elcrispapuh18 <elcrispapuh123@gmail.com>
2022-06-29 19:41:36 -0500

    Sexto Commit

--- conversor.py
+++ conversor.py
@@ -1,3 +1,4 @@
+def conversor (tipo_pesos, valor peso):
 pesos = input ("Cuantos pesos colombianos tienes?")
 pesos = float(pesos)
 valor_dolar =3875
@@ -5,3 +6,23 @@ dolares = pesos / valor_dolar
 dolores = round (dolores, 2)
 dolares = str(dolores)
 print("Tienes $" + dolares + "dolares")
+
+menu = """
+Bienvenido al conversor de monedas 
+
+1 - Pesos mexicanos
+2 - Pesos colombianos
+3 - Pesos argentinos
+
+Elige una opción: """ 
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("mexicanos", 20.34)
+elif opcion == 2:
+    conversor("colombianos", 3905.90)
+elif opcion == 3:
+    conversor("argentinos", 123.16)
+else:
+    print('Ingresa una opción correcta por favor')
